6d56a53197701587db45a41bc39e3cec22686999
Andres Mendoza <andres.david.mm@hotmail.com>
2023-11-19 12:51:59 -0500

    fixed bug when accesing purchases

--- backend/users/serializer.py
+++ backend/users/serializer.py
@@ -4,8 +4,7 @@ from orders.serializer import OrderSerializer
 from .models import User
 
 class UserSerializer(ModelSerializer):
-    cart = CartSerializer(read_only=True, many=True)
-    orders = OrderSerializer(read_only=True, many=True)
+
     class Meta:
         model = User
         fields = ('id', 'email', 'first_name', 'last_name', 'cart', 'orders')
